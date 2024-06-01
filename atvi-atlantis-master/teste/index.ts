8eee34599073ce5bb815a407c953f722d3bfc635
Douglas F. Medeiros <80690245+DouglasMedeiros1@users.noreply.github.com>
2024-06-01 15:17:40 -0300

    Adicionei o Telefone aos dados do Usuario

--- atvi-atlantis-master/teste/index.ts
+++ atvi-atlantis-master/teste/index.ts
@@ -1,5 +1,7 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
+
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
@@ -13,6 +15,13 @@ endereco.estado = `Rio de Janeiro`
 endereco.pais = `Brasil`
 endereco.codigoPostal = `22220-000`
 cliente.endereco = endereco
+let telefone = new Telefone()
+telefone.ddd = `21`
+telefone.numero = `2222-2222`
+cliente.telefones.push(telefone)
+telefone.ddd = `22`
+telefone.numero = `3333-3333`
+cliente.telefones.push(telefone)
 
 let dependente = new Cliente()
 dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
@@ -20,6 +29,7 @@ dependente.nomeSocial = `Princesa Isabel`
 dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
+dependente.telefones = (cliente.telefones as Telefone[])
 dependente.titular = cliente
 cliente.dependentes.push(dependente)
 
